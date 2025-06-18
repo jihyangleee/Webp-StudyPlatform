39cbd0e073d771c7f66fdf5441e9ac1cda8928e4
jihyang <hyanglee0508@naver.com>
2025-06-18 23:36:45 +0900

    changefinal

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
+
 // 미들웨어
 app.use(logger('dev'));
 app.use(express.json());
--- routes/studyR.js
+++ routes/studyR.js
@@ -1,33 +1,93 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
 // 마이페이지 렌더링
 router.get('/mypage', (req, res) => {
-  // 로그인 여부 확인 후 렌더링
-  res.render('mypage');
+  const filePath = path.join(__dirname, '../src/studies.json');
+  let studies = [];
+
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    studies = fileData ? JSON.parse(fileData) : [];
+  }
+
+  res.render('mypage', { studies });
 });
 
 // 스터디 등록 페이지 렌더링
 router.get('/studies/new', (req, res) => {
-  res.render('studyEnroll',{ study: {} } );  
+  res.render('studyEnroll', { study: {} });
 });
 
+// 글 저장
 router.post('/studies', (req, res) => {
   const data = req.body;
-  //!!수정필요!!
-  // data를 JSON 문자열로 변환
-  const jsonData = JSON.stringify(data, null, 2);
+  const filePath = path.join(__dirname, '../src/studies.json');
 
-  // 파일 저장 경로
-  const filePath = path.join(__dirname, 'data', 'study.json');
+  let studies = [];
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    studies = fileData ? JSON.parse(fileData) : [];
+  }
 
-  // 파일에 저장 (덮어쓰기)
+  const newStudy = {
+    id: Date.now().toString(),
+    ...data
+  };
+
+  studies.push(newStudy);
+  const jsonData = JSON.stringify(studies, null, 2);
+
   fs.writeFile(filePath, jsonData, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send('저장 실패');
     }
-    res.send('저장 완료');
+    res.redirect('/studyR/mypage');
   });
+});
+
+// 글 삭제
+router.post('/studies/delete/:id', (req, res) => {
+  const id = req.params.id;
+  const filePath = path.join(__dirname, '../src/studies.json');
+  let studies = [];
+
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    studies = fileData ? JSON.parse(fileData) : [];
+  }
+
+  studies = studies.filter(study => study.id !== id);
+  fs.writeFileSync(filePath, JSON.stringify(studies, null, 2), 'utf8');
+  res.redirect('/studyR/mypage');
+});
+
+// 수정 폼 이동
+router.get('/studies/edit/:id', (req, res) => {
+  const id = req.params.id;
+  const filePath = path.join(__dirname, '../src/studies.json');
+  const fileData = fs.readFileSync(filePath, 'utf8');
+  const studies = JSON.parse(fileData);
+  const study = studies.find(study => study.id === id);
+  if (!study) return res.status(404).send('글 없음');
+  res.render('studyEnroll', { study });
+});
+
+// 수정 반영 저장
+router.post('/studies/edit/:id', (req, res) => {
+  const id = req.params.id;
+  const filePath = path.join(__dirname, '../src/studies.json');
+  const fileData = fs.readFileSync(filePath, 'utf8');
+  let studies = JSON.parse(fileData);
+  const index = studies.findIndex(study => study.id === id);
+  if (index === -1) return res.status(404).send('글 없음');
+
+  studies[index] = { ...studies[index], ...req.body };
+  fs.writeFileSync(filePath, JSON.stringify(studies, null, 2), 'utf8');
+  res.redirect('/studyR/mypage');
 });
 
 module.exports = router;
