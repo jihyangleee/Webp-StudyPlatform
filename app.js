4deb764c162c5987f0ccd4e1bd0aa2d0ccdef20d
jihyang <hyanglee0508@naver.com>
2025-06-20 20:21:39 +0900

    filerepair

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ app.set('view engine', 'ejs');
 
 
 // 미들웨어
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
