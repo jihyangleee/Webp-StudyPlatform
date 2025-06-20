175692bad530e325c437a9caee2e0e172164ad29
jihyang <hyanglee0508@naver.com>
2025-06-20 16:42:42 +0900

    add file

--- app.js
+++ app.js
@@ -4,11 +4,36 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors');
+const multer = require('multer');
+const fs = require('fs');
+const uploadDir = path.join(__dirname, 'uploads');
 
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+  console.log('uploads 폴더 생성 완료');
+}
+// 업로드 경로 및 파일 이름 설정
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'uploads')); // 파일 저장 폴더
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + '-' + file.originalname;
+    cb(null, uniqueName);
+  }
+});
+const upload = multer({ storage: storage });
 const authRouter = require('./routes/authR');
 const usersRouter = require('./routes/users'); // 필요한 경우
 const studyRouter = require('./routes/studyR');
 const app = express();
+
+app.post('/uploads', upload.single('thumbnail'), (req, res) => {
+  console.log('파일 정보:', req.file);
+  res.send('업로드 성공!');
+});
+
+
 
 // 뷰 엔진 설정
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +43,7 @@ app.set('view engine', 'ejs');
 
 
 // 미들웨어
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
